6a938b5d02f01e8293339f470b6897cd2c312e2a
Yugam Amish Patel <yugampatel@gmail.com>
2023-08-28 17:14:32 -0500

    checking

--- script.js
+++ script.js
@@ -10,6 +10,27 @@ function addTask() {
   
     var taskList = document.getElementById("taskList");
     var li = document.createElement("li");
-    li.appendChild(document.createTextNode(task));
+
+    // Create a checkbox
+    var checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+
+    // Create a label for text
+    var label = document.createElement("label");
+    label.appendChild(document.createTextNode(task));
+  
+    // Add an event listener to strike through the text when checked
+    checkbox.addEventListener("change", function() {
+        if (checkbox.checked) {
+            label.style.textDecoration = "line-through";
+        } else {
+            label.style.textDecoration = "none";
+        }
+    });
+
+    // Add the checkbox and label to the list item
+    li.appendChild(label);
+    li.appendChild(checkbox);
+  
     taskList.appendChild(li);
-  }
+}
